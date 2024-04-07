12fbe29850a9716a30ac38f4c136e905c4cc712b
Deron Decamp <decamp.deron@gmail.com>
2024-04-07 19:17:52 -0400

    refactor variable and method names for clarity

--- bx_web_server.js
+++ bx_web_server.js
@@ -4,55 +4,56 @@ import sqlite3 from "sqlite3";
 import argon2 from "argon2";
 import cream from "dotenv";
 import jwt from "jsonwebtoken";
-
+// cash rules everything around me!
 cream.config();
 sqlite3.verbose();
 
 const app = express();
-const db = new sqlite3.Database(process.env.usrdb_path);
+const user_database = new sqlite3.Database(process.env.usrdb_path);
+// status codes
 const STATUS_UNUATHORIZED = 401;
 const STATUS_SERVER_ERROR = 500;
 const STATUS_OK = 200;
 
-app.get("/", (req, res) => {
-  res.send("<h1>hello from blaxstar!</h1>");
+app.get("/", (request_data, response_data) => {
+  response_data.status(STATUS_OK).send("<h1>hello from blaxstar!</h1>");
 });
 
-app.get("/api/login", (req, res) => {
+app.get("/api/login", (request_data, response_data) => {
   // first get the auth header and store it in a variable
-  var ahead = req.header("Authorization");
+  var authorization_header = request_data.header("Authorization");
   // get the b64 cred string as arr by splitting by space, then colon
-  var crd = atob(ahead.split(" ")[1]).split(":");
-  var usr = crd[0];
-  var srt = crd[1];
+  var credentials = atob(authorization_header.split(" ")[1]).split(":");
+  var username = credentials[0];
+  var secret = credentials[1];
 
-  // get the user from the db and verify the srt
-  db.serialize(
+  // get the user from the db and verify the secret
+  user_database.serialize(
     () => {
-      db.get(`SELECT * FROM usr_tbl WHERE username = "${usr}"`, (err, row) => {
-        if (err) {
-          res.send("<h1>USER_NOT_FOUND</h1>");
+      user_database.get(`SELECT * FROM usr_tbl WHERE username = "${username}"`, (query_error, result_row) => {
+        if (query_error) {
+          response_data.status(STATUS_UNUATHORIZED).send({message: "user not found"});
         } else {
-          if (verify_srt(srt, row.srt)) {
-            // generate new tokens if srt is valid
-            var jwt_pld = {
-              iss: `${usr}`,
-              admin: `${row.adm}`,
+          if (verify_secret(secret, result_row.secret)) {
+            // generate new tokens if secret is valid
+            var jwt_payload = {
+              iss: `${username}`,
+              admin: `${result_row.adm}`,
               exp: Math.floor(Date.now() / 1000) + 60 * 1,
             };
             
-            const at = sign_jwt(jwt_pld, "access");
-            const rt = sign_jwt(jwt_pld, "refresh");
+            const access_token = sign_jwt(jwt_payload, "access");
+            const refresh_token = sign_jwt(jwt_payload, "refresh");
 
-            if (at && rt) {
+            if (access_token && refresh_token) {
               // return new tokens
-              res.setHeader("Authorization", `Bearer ${at}`);
-              res.setHeader("X-REF-TOK", rt);
-              res.status(STATUS_OK).send("OK");
+              response_data.setHeader("Authorization", `Bearer ${access_token}`);
+              response_data.setHeader("X-REF-TOK", refresh_token);
+              response_data.status(STATUS_OK).send("OK");
             } else {
               // return error if new tokens could not be generated
-              res.status(STATUS_SERVER_ERROR).send({
-                err: "could not generate token, please try again later.",
+              response_data.status(STATUS_SERVER_ERROR).send({
+                message: "could not generate token, please try again later.",
               });
             }
           }
@@ -62,37 +63,37 @@ app.get("/api/login", (req, res) => {
   );
 });
 
-app.post("/api/rftkn", (req, res) => {
+app.post("/api/rftkn", (request_data, response_data) => {
   // get the auth header and verify refresh token
-  const tkn = req.header("X-REF-TOK");
-  const rt_pld = verify_jwt(tkn, "refresh");
+  const client_token = request_data.header("X-REF-TOK");
+  const client_token_payload = verify_jwt(client_token, "refresh");
   
-  if (rt_pld) {
+  if (client_token_payload) {
     // sign new tokens if the refresh token is valid
-    let new_at = sign_jwt(rt_pld, "access");
-    let new_rt = sign_jwt(rt_pld, "refresh");
+    let new_access_token = sign_jwt(client_token_payload, "access");
+    let new_refresh_token = sign_jwt(client_token_payload, "refresh");
 
-    if (new_at && new_rt) {
+    if (new_access_token && new_refresh_token) {
       // return new tokens
-      res.setHeader("Authorization", `Bearer ${new_at}`);
-      res.setHeader("X-REF-TOK", new_rt);
-      res.status(STATUS_OK).send("TKRF OK");
+      response_data.setHeader("Authorization", `Bearer ${new_access_token}`);
+      response_data.setHeader("X-REF-TOK", new_refresh_token);
+      response_data.status(STATUS_OK).send("TOKEN REFRESH OK");
     } else {
       // return error if new tokens could not be generated
-      res.status(STATUS_SERVER_ERROR).send({
-        err: "could not generate token, please try again later.",
+      response_data.status(STATUS_SERVER_ERROR).send({
+        message: "could not generate token, please try again later.",
       });
     }
   } else {
     // return error if refresh token is invalid
-    res.status(STATUS_UNUATHORIZED).send({
-      err: "invalid refresh token, please log in again.",
+    response_data.status(STATUS_UNUATHORIZED).send({
+      message: "invalid refresh token, please log in again.",
     });
   }
 });
 
-app.get("/api/logout", (req, res) => {
-  res.status(STATUS_OK).send("OK");
+app.get("/api/logout", (request_data, response_data) => {
+  response_data.status(STATUS_OK).send("OK");
 });
 
 app.get("/api/maps", () => {
@@ -103,31 +104,31 @@ app.listen(5000, () => {
   console.log("server listening on port 5000.");
 });
 
-async function verify_srt(srt, hash) {
-  // verify the srt using argon2
-  if (await argon2.verify(hash, srt)) {
+async function verify_secret(request_secret, hashed_secret) {
+  // verify the secret using argon2
+  if (await argon2.verify(hashed_secret, request_secret)) {
     return true;
   }
   return false;
 }
 
-async function hash_srt(srt) {
-  // hash the srt using argon2
+async function hash_secret(secret) {
+  // hash the secret using argon2
   const hash_options = {
     type: argon2.argon2id,
     tagLength: 32,
   };
-  return await argon2.hash(srt, hash_options);
+  return await argon2.hash(secret, hash_options);
 }
 
-function sign_jwt(pld, tkn_type, options) {
+function sign_jwt(token_payload, token_type, options) {
   // check the token type and update the payload accordingly
-  if (tkn_type == "access") {
+  if (token_type == "access") {
     // 1 min access token TODO: change back to 15 min
-    pld.exp = Math.floor(Date.now() / 1000) + 60 * 1;
-  } else if (tkn_type == "refresh") {
+    token_payload.exp = Math.floor(Date.now() / 1000) + 60 * 1;
+  } else if (token_type == "refresh") {
     // 1 min refresh token TODO: change back to 1 hr
-    pld.exp = Math.floor(Date.now() / 1000) + 60 * 1;
+    token_payload.exp = Math.floor(Date.now() / 1000) + 60 * 1;
   } else {
     // invalid token type
     console.log("invalid token type @ sign_jwt ln 119!");
@@ -135,21 +136,21 @@ function sign_jwt(pld, tkn_type, options) {
   }
 
   // create a buffer using the private key and convert it to ascii
-  const pk = Buffer.from(
-    tkn_type == "access" ? process.env.atpk : process.env.rtpk,
+  const private_key = Buffer.from(
+    token_type == "access" ? process.env.atpk : process.env.rtpk,
     "base64"
   ).toString("ascii");
   // sign the payload with the private key
-  return jwt.sign(pld, pk, { ...(options && options), algorithm: "RS256" });
+  return jwt.sign(token_payload, private_key, { ...(options && options), algorithm: "RS256" });
 }
 
-function verify_jwt(tkn, tkn_type) {
+function verify_jwt(token, token_type) {
   // check the token type and use the appropriate key 
-  let tknkey;
-  if (tkn_type == "access") {
-    tknkey = process.env.atpbk;
-  } else if (tkn_type == "refresh") {
-    tknkey = process.env.rtpbk;
+  let rsa_key;
+  if (token_type == "access") {
+    rsa_key = process.env.atpbk;
+  } else if (token_type == "refresh") {
+    rsa_key = process.env.rtpbk;
   } else {
     console.log("invalid token type @ verify_jwt ln 138!");
     return null;
@@ -157,11 +158,11 @@ function verify_jwt(tkn, tkn_type) {
 
   try {
     // create a buffer using the public key and convert it to ascii
-    const pbk = Buffer.from(tknkey, "base64").toString("ascii");
+    const public_key = Buffer.from(rsa_key, "base64").toString("ascii");
     // verify the token with the public key
-    const decoded = jwt.verify(tkn, pbk);
+    const decoded_token = jwt.verify(token, public_key);
     // return the decoded payload
-    return decoded;
+    return decoded_token;
   } catch (e) {
     // log the error
     console.log(e.message ? e.message : e);
